502c72d82867ca94bfbb1d41131914493ffc7a87
Patricio Pagano <pagano.patricio@gmail.com>
2022-05-28 19:16:36 -0300

    Validación de Formulario + modal

--- js/contacto.js
+++ js/contacto.js
@@ -7,7 +7,9 @@ let data = {
   mensajes: [],
 };
 let registro = false;
+var validated = true;
 
+let cantErrors;
 if (registro) {
   comunidad.style.display = flex;
 }
@@ -17,20 +19,26 @@ if (registro) {
 
 //3 Crear función handleSendBtn
 const handleSendBtn = () => {
+  cantErrors = 0;
+  checkFields();
+  if (validated === false) return;
+
   // Capturamos los datos del formulario al hacer click en ENVIAR
   const nuevaPersona = {
     nombre: form.nombre.value,
     apellido: form.apellido.value,
-    correo: form.correo.value,
+    email: form.email.value,
     edad: form.edad.value,
     nacionalidad: form.pais.value,
     genero: form.genero.value,
     fechaIngreso: new Date(),
+    password: form.password.value,
+    confirmarPassword: form.confirmarPassword.value,
   };
 
   // agregamos la nueva persona al objeto de datos
   data = { ...data, personas: [...data.personas, nuevaPersona] };
-
+  console.log(data);
   // limpiamos el formulario
   form.reset();
 
@@ -41,7 +49,6 @@ const handleSendBtn = () => {
       nuevaPersona.fechaIngreso
     )} `
   );
-  //pintamos el DOM con la nueva persona con la función addNewPerson
 
   alert(
     `¡Hola ${nuevaPersona.nombre}! Tu registro se ha realizado exitosamente.\n ¡Felicitaciones! Ya sos parte de la Comunidad Coffee Break`
@@ -70,7 +77,134 @@ function getEdad(dateString) {
   return edad;
 }
 
-sendBtn.addEventListener('click', () => handleSendBtn());
+const checkFields = () => {
+  //capturar los valores ingresados por el usuario
+  const nombre = form.nombre.value.trim();
+  const apellido = form.apellido.value.trim();
+  const email = form.email.value.trim();
+  const edad = form.edad.value;
+  const nacionalidad = form.pais.value;
+  const genero = form.genero.value;
+  const fechaIngreso = new Date();
+  const password = form.password.value.trim();
+  const confirmarPassword = form.confirmarPassword.value.trim();
+
+  // Expresiones Regulares
+  const expRegPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,18}$/;
+
+  //validando campo usuario
+  //(!usuarioValor) ? console.log('CAMPO VACIO') : console.log(usuarioValor)
+  !nombre
+    ? errorValidacion(form.nombre, '*Campo Obligatorio')
+    : validacionOk(form.nombre);
+
+  !apellido
+    ? errorValidacion(form.apellido, '*Campo Obligatorio')
+    : validacionOk(form.apellido);
+
+  //Validación de email
+  !email
+    ? errorValidacion(form.email, '*Campo Obligatorio')
+    : !validarEmail(email)
+    ? errorValidacion(form.email, 'El e-mail no es válido')
+    : validacionOk(form.email);
+  !edad
+    ? errorValidacion(form.edad, '*Campo Obligatorio')
+    : validacionOk(form.edad);
+
+  // console.log(form.genero);
+  // genero === ''
+  //   ? errorValidacion(form.genero, '*Campo Obligatorio')
+  //   : validacionOk(form.genero);
+
+  // Validación de Password
+  !password
+    ? errorValidacion(form.password, '*Campo Obligatorio')
+    : password.length < 8
+    ? errorValidacion(form.password, 'Mínimo 8 caracteres')
+    : !password.match(expRegPassword)
+    ? errorValidacion(
+        form.password,
+        'Debe tener al menos una letra mayúscula, una letra minúscula y un número.'
+      )
+    : validacionOk(form.password);
+
+  //Validación de Confirmación Password
+  !confirmarPassword
+    ? errorValidacion(form.confirmarPassword, 'Confirme su password')
+    : confirmarPassword !== password
+    ? errorValidacion(
+        form.confirmarPassword,
+        'El password ingresado no coincide'
+      )
+    : validacionOk(form.confirmarPassword);
+  return validated;
+};
+
+const errorValidacion = (input, msje) => {
+  const formControl = input.parentElement;
+  const aviso = formControl.querySelector('span');
+  aviso.innerText = msje;
+  formControl.className = 'form-control error';
+  validated = false;
+  cantErrors++;
+};
+const validacionOk = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control ok';
+  if (cantErrors === 0) validated = true;
+};
+
+const validarEmail = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+};
+
+/* MODAL */
+const closeModal = document.querySelector('#closeModal');
+const modalContainer = document.querySelector('#modalContainer');
+const modalContent = document.querySelector('#modalContent');
+
+const modalTitle = document.getElementById('modalTitle');
+const modalText = document.getElementById('modalText');
+const modalText2 = document.getElementById('modalText2');
+
+/* ADD EVENT LISTENER */
+sendBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  // handleSendBtn();
+  console.log(data['personas']);
+  console.log(data['personas'][data['personas'].length - 1]);
+  let lastUser = data['personas'][data['personas'].length - 1];
+  if (validated) {
+    modalContainer.style.opacity = '1';
+    modalContainer.style.visibility = 'visible';
+    modalContent.classList.toggle('modal-close');
+    modalTitle.innerHTML = `¡Felicitaciones!`;
+    modalText.innerHTML = `¡Hola Patricio! Tu registro se ha realizado exitosamente.`;
+    modalText2.innerHTML = 'Ya sos parte de la Comunidad Coffee Break';
+  }
+});
+
+closeModal.addEventListener('click', () => {
+  modalContent.classList.toggle('modal-close');
+
+  setTimeout(() => {
+    modalContainer.style.opacity = '0';
+    modalContainer.style.visibility = 'hidden';
+  }, 500);
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modalContainer) {
+    modalContent.classList.toggle('modal-close');
+    setTimeout(() => {
+      modalContainer.style.opacity = '0';
+      modalContainer.style.visibility = 'hidden';
+    }, 500);
+  }
+});
 
 const videoCards = [...document.querySelectorAll('.video-card')];
 
